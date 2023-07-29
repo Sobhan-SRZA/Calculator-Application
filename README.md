84621eb190d79bfdb8a77652432dc5d32f5279b9
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2023-07-29 10:39:59 +0330

    Calculator App v1.0.0

--- README.md
+++ README.md
@@ -5,7 +5,11 @@
 - first of all open `install.cmd` to install all libraries.
 - open cmd from source path then send this `python src/main.py`
 
+# Screen shots
+<img src="">
+
 Enjoy ❤
+
 Build with love and hard work
 
 © Sobhan-SRZA
--- src/main.py
+++ src/main.py
@@ -1,28 +1,110 @@
 import sys
-from PyQt6 import QtGui
-from PyQt6.QtGui import QFont
-from PyQt6.QtWidgets import QLabel, QWidget, QVBoxLayout, QApplication
-from PyQt6.QtCore import QTimer, Qt
+from PyQt6 import (QtGui)
+from PyQt6.QtGui import (QFont, QIcon)
+from PyQt6.QtWidgets import (QLabel, QVBoxLayout, QApplication, QGridLayout, QHBoxLayout, QLineEdit, QMainWindow, QPushButton, QWidget)
+from PyQt6.QtCore import (QTimer, Qt)
+from functools import (partial)
 
-class Window(QWidget):
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        layout = QVBoxLayout()
+        self.main_layout = QVBoxLayout()
+        self.setWindowTitle("Calculator")
+        self.setWindowIcon(QIcon('favicon.png'))
+        self.setLayout(self.main_layout)
+        central_widget = QWidget(self)
+        central_widget.setLayout(self.main_layout)
+        self.setCentralWidget(central_widget)
+        self.create_output()
+        self.create_numpad()
+        self.show()
         self.label = QLabel()
+        self.label.setText("© Sobhan-SRZA")
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.label.setFont(QFont("calibri", 40, -1, False))
-        layout.addWidget(self.label)
-        self.newLabel = QLabel()
-        self.newLabel.setText("© Sobhan-SRZA")
-        self.newLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.newLabel.setFont(QFont("arial", 20, -1, False))
-        layout.addWidget(self.newLabel)
-        self.setWindowTitle("Calculator Application")
-        self.setWindowIcon(QtGui.QIcon('icon.png'))
-        self.setGeometry(100, 100, 800, 400)
-        self.setLayout(layout)
-        
-app = QApplication(sys.argv)
-window = Window()
-window.show()
-app.exec()
+        self.label.setFont(QFont("arial", 10, -1, False))
+        self.main_layout.addWidget(self.label)
+
+    def create_output(self):
+        panel = QHBoxLayout()
+        self.display = QLineEdit("0")
+        self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.display.setReadOnly(True)
+        panel.addWidget(self.display)
+        backspace = QPushButton("⌫")
+        backspace.clicked.connect(self.on_backspace)
+        panel.addWidget(backspace)
+        self.main_layout.addLayout(panel)
+
+    def create_numpad(self):
+        buttons = [
+            ["7", "8", "9"],
+            ["4", "5", "6"],
+            ["1", "2", "3"],
+            [".", "00", "0"],
+            ["+", "-", "//"],
+            ["*", "/", "^"],
+            ["%", "Clear", "="],
+        ]
+
+        numpad = QGridLayout()
+
+        for i, row in enumerate(buttons):
+            for j, button in enumerate(row):
+                btn = QPushButton(button)
+                btn.clicked.connect(partial(self.on_click, button))
+                numpad.addWidget(btn, i, j)
+
+        self.main_layout.addLayout(numpad)
+
+    def on_backspace(self):
+        edit = self.get_display()[:-1]
+        if edit == "" or edit.startswith("ERROR: "):
+            self.set_display("0")
+        else:
+            self.set_display(edit)
+
+    def on_click(self, button):
+        match button:
+            case "=":
+                item = self.get_display().replace("^", "**")
+                result = 0
+                try:
+                    result = eval(item)
+                except SyntaxError:
+                    self.set_display("ERROR: Invalid input")
+                except ZeroDivisionError:
+                    self.set_display("ERROR: Division by zero")
+                else:
+                    self.set_display(str(result))
+            case "00":
+                if self.get_display().startswith("ERROR: "):
+                    self.set_display("0")
+                elif self.get_display() != "0":
+                    self.set_display(self.get_display() + "00")
+            case "Clear":
+                self.set_display("0")
+            case "%":
+                self.set_display(self.get_display() + "%")
+            case _:
+                if self.get_display() == "0" or self.get_display().startswith("ERROR: "):
+                    self.set_display(button)
+                else:
+                    self.set_display(self.get_display() + button)
+
+    def get_display(self) -> str:
+        return self.display.text()
+
+    def set_display(self, text: str):
+        self.display.setText(text)
+
+def main():
+    app = QApplication(sys.argv)
+    app.setApplicationName("org.codeberg.Grafcube.Calculator")
+    app.setStyle('Fusion')
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
